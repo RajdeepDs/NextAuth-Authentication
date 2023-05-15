65961c9cf2f938211e1a50dfde9b27e5847a8556
Rajdeep Das <rajdeepds626@gmail.com>
2023-05-15 21:04:53 +0530

    feat: database using prisma added

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,6 +1,9 @@
-import NextAuth from "next-auth";
+import { PrismaAdapter } from "@next-auth/prisma-adapter";
+import NextAuth, { NextAuthOptions } from "next-auth";
 import GithubProvider from "next-auth/providers/github";
-export const authOptions = {
+import { db } from "@/lib/db";
+export const authOptions: NextAuthOptions = {
+  adapter: PrismaAdapter(db as any),
   providers: [
     GithubProvider({
       clientId: process.env.NEXTAUTH_CLIENT_ID!,
@@ -10,7 +13,37 @@ export const authOptions = {
   pages: {
     signIn: "/signin",
   },
+  session: {
+    strategy: "jwt",
+  },
+  callbacks: {
+    async session({ token, session }) {
+      return session;
+    },
+    async jwt({ token, user }) {
+      const dbUser = await db.user.findFirst({
+        where: {
+          email: token.email,
+        },
+      });
+
+      if (!dbUser) {
+        if (user) {
+          token.id = user?.id;
+        }
+        return token;
+      }
+
+      return {
+        id: dbUser.id,
+        name: dbUser.name,
+        email: dbUser.email,
+        picture: dbUser.image,
+      };
+    },
+  },
 };
+
 const handler = NextAuth(authOptions);
 
 export { handler as GET, handler as POST };
